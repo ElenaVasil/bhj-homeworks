38b5ad55f90f0d6bfa2aab4b49d93e85ca68a217
Elena <marsusha@yandex.ru>
2022-09-06 21:27:28 +0300

    ReSolved cart

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -19,31 +19,14 @@ btnAdd.forEach((btn) => btn.addEventListener('click', (e) => {
   let img = e.target.closest('.product').querySelector('.product__image').src;
   let count = e.target.closest('.product').querySelector('.product__quantity-value').textContent;
   let products = Array.from(document.getElementsByClassName('cart__product'));
-  let isInCart = false;
 
-  products.forEach((product) => {
-    if (product.getAttribute('data-id') === id) {
-      product.querySelector('.cart__product-count').textContent = parseInt(product.querySelector('.cart__product-count').textContent) + parseInt(count);
-      isInCart = true;
-    }
-  });
-
-  if (!isInCart) {
-    cart.innerHTML += `<div class="cart__product" data-id=` + id + `>
-      <img class="cart__product-image" src=`+ img + `>
-      <div class="cart__product-count">` + count + `</div>
-      </div>`
-  }
-  /*
-  let isInCart = products.find((product)=> {
-    product.dataset.id === id;
-  });
-  if (isInCart) {
-    products.querySelector('.cart__product-count').textContent = parseInt(products.querySelector('.cart__product-count').textContent) + parseInt(count);
+  let productInCart = products.find(product=> product.dataset.id === id);
+  if (productInCart) {
+    productInCart.querySelector('.cart__product-count').textContent = parseInt(productInCart.querySelector('.cart__product-count').textContent) + parseInt(count);
   } else {
     cart.innerHTML += `<div class="cart__product" data-id=` + id + `>
       <img class="cart__product-image" src=`+ img + `>
       <div class="cart__product-count">` + count + `</div>
       </div>`
-  }   */  
+  }    
 }));
